d75925c2fa1cf3daab0df42504344e71680c5a28
Luke McInerney <mciluke@gmail.com>
2023-01-21 15:31:45 -0500

    add color hints to letterbox

--- src/components/LetterBox.vue
+++ src/components/LetterBox.vue
@@ -3,9 +3,16 @@ const props = defineProps({
   letter: {
     type: String,
     default: ""
+  },
+  color: {
+    type: String,
+    default: ""
   }
 })
 </script>
 <template lang="pug">
-div(class="uppercase border-2 border-gray-200 col-span-1 flex items-center justify-center h-16") {{ letter }} 
+div(
+  class="uppercase border-2 border-gray-200 col-span-1 flex items-center justify-center h-16 transition-all duration-300",
+  :class="{'border-gray-500 bg-gray-500 text-white' : color == 'gray', 'border-green-600 bg-green-600 text-white' : color == 'green','border-yellow-500 bg-yellow-500 text-white' : color == 'yellow'}"
+) {{ letter }} 
 </template>
--- src/components/WordRow.vue
+++ src/components/WordRow.vue
@@ -23,6 +23,16 @@ watch(
           letterPool.push(props.solution.charAt(i))
         }
       }
+      for (let i = 0; i < 5; i++) {
+        if (temp[i] == "gray" && letterPool.indexOf(props.value.charAt(i)) != -1) {
+          letterPool.splice(letterPool.indexOf(props.value.charAt(i)), 1);
+          temp[i] = "yellow"
+        }
+        colors.value[i] = temp[i];
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, 500));
+
     }
   }
 )
@@ -31,5 +41,10 @@ watch(
 
 <template lang="pug">
 div(class="grid max-w-xs grid-cols-5 gap-1 mx-auto mb-1")
-  LetterBox(v-for="i in 5", :key="i", :letter="value[i - 1]")
+  LetterBox(
+    v-for="i in 5", 
+    :key="i", 
+    :letter="value[i - 1]",
+    :color="colors[i - 1]"
+  )
 </template>
